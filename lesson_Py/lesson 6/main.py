193078bcf56a9caec9e8e8ea2641b0eb5c0c9a7b
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2024-03-07 11:21:58 +0500

    update

--- lesson_Py/lesson 6/main.py
+++ lesson_Py/lesson 6/main.py
@@ -27,21 +27,28 @@
 # second_num = int(input("2-soni kiriting"))
 # print(count_multiple(first_num , second_num))
 
-def calc(a,b,op):
-    if int(op) == 1:
-        print(f"ayrma : {a - b}")
-    elif int(op) == 2:
-        print(f"ko'paytma : {a * b}")
-    elif int(op) == 3:
-        print(f"bo'linma : {a / b}")
-    else:
-        print(f"yig'indi : {a + b}")
+# def calc(a,b,op):
+#     if int(op) == 1:
+#         print(f"ayrma : {a - b}")
+#     elif int(op) == 2:
+#         print(f"ko'paytma : {a * b}")
+#     elif int(op) == 3:
+#         if b > 0:
+#             print(f"bo'linma : {a / b}")
+#     else:
+#         print(f"yig'indi : {a + b}")
 
-while True:
-    a = int(input("1 - sonni kiriting"))
-    b = int(input("2 - sonni kiriting"))
-    c = input("qanday amal bajariladi:\n0 - dasturni to'xtatish\n1 - ayrish\n2 - ko'paytirish\n3 - bo'lish\nboshqa holda qo'shish")
-    calc(a, b, c)
-    if int(c) == 0:
-        print("dastur to'xtatildi")
-        break
+# while True:
+#     a = int(input("1 - sonni kiriting"))
+#     b = int(input("2 - sonni kiriting"))
+#     c = input("qanday amal bajariladi:\n0 - dasturni to'xtatish\n1 - ayrish\n2 - ko'paytirish\n3 - bo'lish\nboshqa holda qo'shish")
+#     calc(a, b, c)
+#     if int(c) == 0:
+#         print("dastur to'xtatildi")
+#         break
+
+son = int(input("son"))
+
+while son % 5 == 0:
+    son /= 5
+print(son == 1)
