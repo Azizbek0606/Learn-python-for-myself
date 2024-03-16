bc44668d98833437a70f80ceca47ddc57bbb6c05
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2024-03-16 11:51:27 +0500

    update

--- homework_Py/10ta_10tadan/foor_loop/task_1.py
+++ homework_Py/10ta_10tadan/foor_loop/task_1.py
@@ -1,1 +1,5 @@
-print("hello world1")
+a = int(input("a son kiriting"))
+b = int(input("b son kiriting"))
+
+for i in range(a):
+    print(b)
--- homework_Py/10ta_10tadan/foor_loop/task_2.py
+++ homework_Py/10ta_10tadan/foor_loop/task_2.py
@@ -1,1 +1,8 @@
-print("hello world2")
+a = int(input("a son kiriting"))
+b = int(input("b son kiriting"))
+count = 0
+if b < a:
+    for i in range(b , a + 1):
+        count += 1
+        print(f"sonlar: {i}")
+print(f"sonlar soni: {count}")
