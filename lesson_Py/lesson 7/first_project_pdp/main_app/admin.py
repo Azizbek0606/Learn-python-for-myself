4ba5c0413b783271cc63212d985ba2e99ed9a16b
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2024-03-29 13:53:48 +0500

    update

--- lesson_Py/lesson 7/first_project_pdp/main_app/admin.py
+++ lesson_Py/lesson 7/first_project_pdp/main_app/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import Article
-# Register your models here.
+from .models import *
 
+admin.site.register(Categories)
 admin.site.register(Article)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(View)
--- lesson_Py/lesson 7/first_project_pdp/main_app/models.py
+++ lesson_Py/lesson 7/first_project_pdp/main_app/models.py
@@ -1,8 +1,42 @@
 from django.db import models
 
 # Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length=100)
+    def __str__(self):
+        return str(self.name)
 class Article(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     date = models.DateField(auto_now_add=True)
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    image   = models.ImageField(blank=True , upload_to="blog/")
+    updated = models.DateTimeField(auto_now=True)
+    likes = models.IntegerField(default=0)
+    comments = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
+    category = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    def __str__(self):
+        return str(self.title)
+
+class Comment(models.Model):
+    comment = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    def __str__(self):
+        return str(self.comment)
+
+class Like(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    def __str__(self):
+        return str(self.author)
+
+class View(models.Model):
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    def __str__(self):
+        return str(self.author)
