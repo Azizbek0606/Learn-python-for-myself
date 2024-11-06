445f645d78be88790eeb45cfb9afff4eb83e7a84
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2024-11-06 09:14:14 +0500

    update

--- second_course/lesson/lesson_4/main.py
+++ second_course/lesson/lesson_4/main.py
@@ -1,17 +1,51 @@
-class Cats:
-    name = "Alex"
-    age = 2
-    color = "white"
+# class Cats:
+#     name = "Alex"
+#     age = 2
+#     color = "white"
 
-    def set_name(self, name):
-        """example class function"""
-        print("call method from cats object")
+#     def set_name(self, name):
+#         """example class function"""
+#         print("call method from cats object")
+#         self.name = name
+#         print(f"this is {self.name}")
+
+#     def voice(self):
+#         print(f"Meow {self.name}")
+
+#     def get_name(self):
+#         return self.color, self.age ,self.name
+
+# ct = Cats()
+# getattr(ct , "name")
+# setattr(ct , "age" , 40)
+# ct.set_name("Sara")
+# print(hasattr(ct, "color"))
+# delattr(ct, "color")
+# ct.voice()
+
+
+class Dogs:
+
+    name = None
+    age = None
+    wight = None
+    breed = None
+
+    def set_value(self, name, age, wight, breed):
         self.name = name
-        print(f"this is {self.name}")
+        self.age = age
+        self.wight = wight
+        self.breed = breed
 
-    def voice(self):
-        print(f"Meow {self.name}")
+        if self.wight is not None:
+            self.bark()
 
-ct = Cats()
-ct.set_name("Sara")
-ct.voice()
+    def get_attr(self):
+        return self.name, self.age, self.wight, self.breed
+
+    def bark(self):
+        print(f"woff woff I'm {self.breed}")
+
+
+dog = Dogs()
+dog.set_value("Buddy", 3, 20, "German Shepherd")
