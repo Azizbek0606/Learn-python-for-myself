b2264226b262992f27f6ca1b154fec56b9990660
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2024-11-13 11:33:07 +0500

    update

--- second_course/lesson/lesson_9/main.py
+++ second_course/lesson/lesson_9/main.py
@@ -31,12 +31,33 @@
 # print(res)
 
 
-class Point:
-    def __init__(self, *args):
-        self.__coords = args
+# class Point:
+#     def __init__(self, *args):
+#         self.__coords = args
+
+#     def __abs__(self):
+#         return list(map(abs, self.__coords))
+
+# pt = Point(-1,2,-4,-3,8,-5,6,-7)
+# print(abs(pt))
+
+
+class Number:
+    def __init__(self, num):
+        self.num = num
+
+    def __str__(self):
+        return str(self.num)
+
+    def __repr__(self):
+        return f"{self.__class__}: {self.num}"
+
+    def __len__(self):
+        return len(str(abs(self.num)))
 
     def __abs__(self):
-        return list(map(abs, self.__coords))
-    
-pt = Point(-1,2,-4,-3,8,-5,6,-7)
-print(abs(pt))
+        return abs(self.num)
+
+
+nr = Number(-5)
+print(len(nr))
